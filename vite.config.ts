b615bce90044776fb2523269c1a5ff927c3546b3
Emanuel0428 <cod2877@gmail.com>
2025-04-19 13:18:07 -0500

    Se agregó compatibilidad con PWA y nuevos recursos para Clinica Smiley
    - Se agregaron imágenes del logotipo de emoticonos en formatos PNG y WEBP.
    - Se creó un archivo de manifiesto de la aplicación web para la configuración de PWA.
    - Se agregaron iconos de manifiesto de la aplicación web de diferentes tamaños.
    - Se actualizó la configuración de Vite para incluir el complemento de PWA con la configuración del manifiesto.
    - Se modificó ligeramente el formato del archivo main.tsx.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Clinica Smiley',
+        short_name: 'Clinica Smiley',
+        description: 'Clinica Smiley - App de agendamientos',
+        theme_color: '#80ffbb',
+        background_color: '#80ffbb',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+    }),
+  ],
   server: {
     port: 5173,
   },
